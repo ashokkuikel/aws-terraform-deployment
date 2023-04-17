eadab430b237c310f511e9ae8ebf77158a10cd71
Ashok kuikel <mail.ashokkuikel@gmail.com>
2023-04-16 22:40:24 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -116,6 +116,48 @@ resource "aws_db_instance" "wordpress" {
   skip_final_snapshot = true
 }
 
+
+resource "aws_cloudwatch_log_group" "main" {
+  name = local.app_name
+}
+
+resource "aws_lb" "main" {
+  name               = "wordpress-alb"
+  internal           = false
+  load_balancer_type = "application"
+  subnets            = aws_subnet.private.*.id
+
+  security_groups = [aws_security_group.ecs_service.id]
+}
+
+resource "aws_lb_target_group" "main" {
+  name     = "wordpress-tg"
+  port     = 80
+  protocol = "HTTP"
+  vpc_id   = aws_vpc.main.id
+
+  health_check {
+    enabled             = true
+    interval            = 30
+    path                = "/"
+    port                = "traffic-port"
+    healthy_threshold   = 3
+    unhealthy_threshold = 3
+    timeout             = 5
+  }
+}
+
+resource "aws_lb_listener" "main" {
+  load_balancer_arn = aws_lb.main.arn
+  port              = 80
+  protocol          = "HTTP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.main.arn
+  }
+}
+
 resource "aws_db_subnet_group" "main" {
   name       = "main"
   subnet_ids = aws_subnet.private.*.id
