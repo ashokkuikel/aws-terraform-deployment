5a9fb76f68542a737fec1f4c0ff48208f4ae2e3d
Ashok kuikel <mail.ashokkuikel@gmail.com>
2023-04-16 23:08:01 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -118,26 +118,27 @@ resource "aws_db_instance" "wordpress" {
 
 resource "aws_s3_bucket" "wp_content" {
   bucket = "${local.app_name}-wp-content"
-  
-  lifecycle_rule {
-    id      = "versioning"
-    status  = "Enabled"
-    prefix  = ""
-    enabled = true
-  }
 
   lifecycle {
     ignore_changes = [
       # Ignore changes to the RDS instance's tags
-      "tags",
+      tags,
       # Ignore changes to the wp-content S3 bucket's versioning configuration
-      "lifecycle_rule",
+      lifecycle_rule {
+        id      = "versioning"
+        prefix  = ""
+        enabled = true
+        noncurrent_version_expiration {
+          days = 30
+        }
+      }
     ]
 
     # Automatically delete all resources when they are removed from the Terraform configuration
     prevent_destroy = false
   }
 }
+
 
 
 # Create a resource to delete all resources except RDS and wp-content S3 bucket
